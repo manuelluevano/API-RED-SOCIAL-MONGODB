f0d0871f4066c83d4495255795020e9d5454dea3
Manuel  Luevano <manuelluevano@hotmail.com>
2023-08-10 03:02:37 -0600

    deploy test 3

--- db/conexion.js
+++ db/conexion.js
@@ -4,8 +4,8 @@ require("dotenv").config({ path: "variables.env" });
 const connection = async () => {
   mongoose.Promise = global.Promise;
   mongoose
-    .connect(process.env.MONGO_DB_URL || process.env.DB_URL)
-    .then((res) => console.log("Conectado a la base de datos", res.connections[0].db.s.namespace.db))
+   .connect(process.env.MONGO_DB_URL || process.env.DB_URL)
+    .then(() => console.log("Conectado a la base de datos"))
     .catch((error) => console.log(error));
 };
 
